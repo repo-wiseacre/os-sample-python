3fd50b02f82c22bc7c3a046523aca0f8f2aba6ae
Shuvajit Ghosh <ghosh.shuvajit@gmail.com>
2020-07-10 13:55:04 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,26 @@
 from flask import Flask
-application = Flask(__name__)
+import requests
+import json
 
-@application.route("/")
+
+app = Flask(__name__)
+
+
+@app.route('/')
 def hello():
-    return "Hello World!"
+    response  = requests.get("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02")
+    print("Hello world!")
+    print(response.json())
+    weatherreport = response.json()
 
-if __name__ == "__main__":
-    application.run()
+    cords = weatherreport["coord"]
+    print(cords["lon"],"aaaaaa")
+
+    html_snippet = "<head><title>HTML in 10 Simple Steps or Less</title><meta http-equiv='refresh' content='5' /></head><body><table><tr><td>weather cords</td></tr><tr><td>longitude: </td><td>{var1:}</td></tr><tr><td>lattitude: </td><td>{var2:}</td></tr></table></body>"
+
+    print()
+
+    return html_snippet.format(var1=cords["lon"], var2=cords["lat"])
+
+if __name__ == '__main__':
+    app.run()
