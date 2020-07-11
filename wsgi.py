03f5aca511e62bdc7b97eed50ddbca41ff50179d
Shuvajit Ghosh <ghosh.shuvajit@gmail.com>
2020-07-11 19:42:48 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    print("hello called....")
     response  = requests.get("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02")
     print("Hello world!")
     print(response.json())
