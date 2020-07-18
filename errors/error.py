95ef4430a7f42978044e73ec28222763827f96f4
Shuvajit Ghosh <ghosh.shuvajit@gmail.com>
2020-07-18 18:20:44 +0530

    Create error.py

---
+++ errors/error.py
@@ -0,0 +1,5 @@
+"HTTP error code 400":"Bad Request The server cannot or will not process the request due to an apparent client error (e.g., malformed request syntax, size too large, invalid request message framing, or deceptive request routing)"
+"HTTP error code 405":"Method Not Allowed A request method is not supported for the requested resource; for example, a GET request on a form that requires data to be presented via POST, or a PUT request on a read-only resource."
+"HTTP error code 500":"The 500 Internal Server Error is a very general HTTP status code that means something has gone wrong on the web site's server but the server could not be more specific on what the exact problem is"
+"HTTP error code 200":"success"
+"HTTP error code [link]":https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
